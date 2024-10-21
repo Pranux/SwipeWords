875e2db3fd5897b62dc7b0ede3da963b4ec67a5c
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-10-22 00:43:50 +0300

    Fixed a bug, where user couldn't play the game

---
+++ SwipeWords/FlashcardMigrations/20241009214727_AddedFlashcardStorage.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwipeWords.FlashcardMigrations
+{
+    /// <inheritdoc />
+    public partial class AddedFlashcardStorage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flashcards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CorrectWords = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IncorrectWords = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flashcards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flashcards");
+        }
+    }
+}
